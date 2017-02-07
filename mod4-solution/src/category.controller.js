290d956755be38610fb69150088d1113c42c6ba8
Yemi Adewale <yaadewale55@gmail.com>
2017-02-07 11:45:41 -0600

    Initial Commit

---
+++ mod4-solution/src/category.controller.js
@@ -0,0 +1,13 @@
+
+
+(function () {
+    'use strict';
+
+    angular.module('MenuApp')
+        .controller('CategoryController', CategoryController);
+
+    CategoryController.$inject = ['categoriesList'];
+    function CategoryController(categoriesList) {
+        this.categoriesList = categoriesList;
+    }
+})()
